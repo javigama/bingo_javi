a7f7832473feffd972f48f452736a0c0ebd3e3f9
Javi <javigama@yahoo.es>
2014-11-05 20:03:06 +0100

    Generado código php para el funcionamiento de los botones

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
+<?php
+	
+	session_name('Bingo_Virtual');
+	session_start();
+
+	if (isset($_POST['nuevo_juego'])) {
+		session_destroy();
+		session_start();
+	}
+
+	if (!isset($_SESSION['generados'])) {
+		$_SESSION['generados'] = array();
+	}
+
+	if (isset($_POST['obtener_numero'])) {
+		do {
+           	$n = rand(1, 90);
+        } while (in_array($n, $_SESSION['generados'])); 
+		array_push($_SESSION['generados'], $n);
+	}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 	<head>
@@ -5,26 +28,28 @@
 		<title>Bingo virtual</title>
 	</head>
 	<body>
+		<form action="index.php" method="post">
 <?php
-		echo '<table align="center" border="1">';
-		for ($i=0; $i<11; $i++) {
-			echo '<tr>';
-			for ($j=0; $j<9; $j++) {
-				if (($j == 0 && $i == 9) || ($j <> 8 && $i == 10))
-					echo '<td></td>';
-				else {
-					if ($j == 0)
-						$k = $j*10 + $i + 1;
-					else
-						$k = $j*10 + $i;
-					echo '<td>'.$k.'</td>';
+			echo '<table align="center" border="1">';
+			for ($i=0; $i<11; $i++) {
+				echo '<tr>';
+				for ($j=0; $j<9; $j++) {
+					if (($j == 0 && $i == 9) || ($j <> 8 && $i == 10))
+						echo '<td></td>';
+					else {
+						if ($j == 0)
+							$k = $j*10 + $i + 1;
+						else
+							$k = $j*10 + $i;
+						echo '<td>'.$k.'</td>';
+					}
 				}
+				echo '</tr>';
 			}
-			echo '</tr>';
-		}
-		echo '</table>';
+			echo '</table>';
 ?>
-		<button type="submit" name="obtener_numero">Obtener numero</button>
-		<button type="submit" name="nuevo_juego">Nuevo juego</button>
+			<button type="submit" name="obtener_numero">Obtener numero</button>
+			<button type="submit" name="nuevo_juego">Nuevo juego</button>
+		</form>
 	</body>
 </html>
