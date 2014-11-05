017765405b1129133f22e00e73ea63751a7cc5cc
Javi <javigama@yahoo.es>
2014-11-05 19:19:50 +0100

    Creada tabla con los numeros del bingo

--- public/index.php
+++ public/index.php
@@ -5,5 +5,24 @@
 		<title>Bingo virtual</title>
 	</head>
 	<body>
+<?php
+		echo '<table align="center" border="1">';
+		for ($i=0; $i<11; $i++) {
+			echo '<tr>';
+			for ($j=0; $j<9; $j++) {
+				if (($j == 0 && $i == 9) || ($j <> 8 && $i == 10))
+					echo '<td></td>';
+				else {
+					if ($j == 0)
+						$k = $j*10 + $i + 1;
+					else
+						$k = $j*10 + $i;
+					echo '<td>'.$k.'</td>';
+				}
+			}
+			echo '</tr>';
+		}
+		echo '</table>';
+?>
 	</body>
 </html>
